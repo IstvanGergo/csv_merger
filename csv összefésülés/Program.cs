425980178189ac61225eca33da647fc5512086e5
IstvanGergo <istvangergo96@gmail.com>
2022-12-16 22:12:38 +0100

    Brought the file checking out from the class to there.

--- csv összefésülés/Program.cs
+++ csv összefésülés/Program.cs
@@ -4,8 +4,22 @@
     {
         static void Main(string[] args)
         {
-
-            csvfésü asd = new();
+            string csv1 = "C:\\csv\\procurement_list.csv";
+            string csv2 = "C:\\csv\\procurement_list (1).csv";
+            /*Fájlellenörzés, létezik-e a megadott helyen, megadott nevű fájl*/
+            while (!File.Exists(csv1))
+            {
+                Console.WriteLine("Az első fájl helye, vagy neve nem megfelelő!\n" +
+                    "Pontos hely megadása:\nPl.:C:\\\\csv\\\\procurement list.csv");
+                csv1 = Console.ReadLine()!;
+            }
+            while (!File.Exists(csv2))
+            {
+                Console.WriteLine("A második fájl helye, vagy neve nem megfelelő!\n" +
+                    "Pontos hely megadása:\nPl.:C:\\\\csv\\\\procurement list (1).csv");
+                csv2 = Console.ReadLine()!;
+            }
+            csvfésü asd = new(csv1, csv2);
 
         }
     }
